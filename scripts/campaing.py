10f75e83dd3ac9ca52795a94be8ba234aaf4f456
Higlik <victor.silva.franca18@gmail.com>
2024-01-09 17:59:11 -0300

    Add decison buttuon

--- scripts/campaing.py
+++ scripts/campaing.py
@@ -1,7 +1,9 @@
 import pygame, sys
 from utils.fogbutton import FogButton
+from utils.button import Button
 import utils.language as lang
 import utils.config as config
+
 
 class Campaing:
     def __init__(self, screen, screen_rect, fps, resolution):
@@ -11,22 +13,15 @@ class Campaing:
         self.resolution = resolution
         self.clock = pygame.time.Clock()
 
-    def campaing(self):
+    def run(self):
         dict_lang = lang.Language.set_lang(self, config.language)
     
         # load background images
         menu_background = pygame.transform.scale(
             pygame.image.load('assets/background/Map1.jpg').convert(),
             self.resolution
-        )
-
-        # load PopUp
-        show_popup = pygame.transform.scale(
-            pygame.image.load('asstes/fogbuttonpopup.png').convert(),
-            (1000,700)
-        )
-        
-
+        )        
+    
         # menu text variables
         font_level = pygame.font.Font("assets/fonts/alagard.ttf", 20)        
         menu_text = font_level.render(dict_lang["campaing_text_level"], True, "#11ebeb")
@@ -38,46 +33,63 @@ class Campaing:
         # menu FogButton text variables
         menu_text_color = "#ff0000"
         hover_text_color = "White"
-        image = pygame.image.load("assets/buttons/zone_buttons/Zone_Layout.png")
+        fogbutton_image = pygame.image.load('assets/buttons/zone_buttons/Zone_Layout.png')
 
         #Zones FogButton
-        zone1_FogButton = FogButton(image, (150, 100), "Explorar", font, menu_text_color, hover_text_color)
-        zone2_FogButton = FogButton(image, (500, 100), "Explorar", font, menu_text_color, hover_text_color)
-        zone3_FogButton = FogButton(image, (850, 100), "Explorar", font, menu_text_color, hover_text_color)
-        zone4_FogButton = FogButton(image, (150, 350), "Explorar", font, menu_text_color, hover_text_color)
-        zone6_FogButton = FogButton(image, (850, 350), "Explorar", font, menu_text_color, hover_text_color)
-        zone7_FogButton = FogButton(image, (150, 600), "Explorar", font, menu_text_color, hover_text_color)
-        zone8_FogButton = FogButton(image, (500, 600), "Explorar", font, menu_text_color, hover_text_color)
-        zone9_FogButton = FogButton(image, (850, 600), "Explorar", font, menu_text_color, hover_text_color)
+        zone1_FogButton = FogButton(fogbutton_image, (150, 100), "Explorar", font, menu_text_color, hover_text_color)
+        zone2_FogButton = FogButton(fogbutton_image, (500, 100), "Explorar", font, menu_text_color, hover_text_color)
+        zone3_FogButton = FogButton(fogbutton_image, (850, 100), "Explorar", font, menu_text_color, hover_text_color)
+        zone4_FogButton = FogButton(fogbutton_image, (150, 350), "Explorar", font, menu_text_color, hover_text_color)
+        zone6_FogButton = FogButton(fogbutton_image, (850, 350), "Explorar", font, menu_text_color, hover_text_color)
+        zone7_FogButton = FogButton(fogbutton_image, (150, 600), "Explorar", font, menu_text_color, hover_text_color)
+        zone8_FogButton = FogButton(fogbutton_image, (500, 600), "Explorar", font, menu_text_color, hover_text_color)
+        zone9_FogButton = FogButton(fogbutton_image, (850, 600), "Explorar", font, menu_text_color, hover_text_color)
         
+        # load PopUp
+        show_popup = pygame.transform.scale(
+            pygame.image.load('assets/popups/fogbuttonpopup.png').convert(),
+            (1000,700)
+        )
+        
+        #load decision button
+        decisio_button_image =  pygame.image.load('assets/buttons/decision_button.png')
+        decisio_button_search = Button(decisio_button_image, (450,500), "Vasculhar",font,menu_text_color,hover_text_color)
+        decisio_button_leave = Button(decisio_button_image, (580,500), "Sair", font, menu_text_color, hover_text_color)
+
+        popup_validator = False
+
         while True:
             # set frames
             self.clock.tick(self.fps)
             
             Menu_mouse_position = pygame.mouse.get_pos()
             
-            # draw background
-            self.screen.blit(menu_background, (0, 0))
-
-            # draw menu text
-            self.screen.blit(menu_text, menu_text_rect)
-
-            # draw FogButton
-            for Fogbutton in [
-                zone1_FogButton,
-                zone2_FogButton,
-                zone3_FogButton,
-                zone4_FogButton,
-                zone6_FogButton,
-                zone7_FogButton,
-                zone8_FogButton,
-                zone9_FogButton]:
-                Fogbutton.changeColor(Menu_mouse_position)
-                if Fogbutton.disable_button == False:
-                    Fogbutton.update(self.screen)
-
-
-
+            if popup_validator == False:
+                # draw background
+                self.screen.blit(menu_background, (0, 0))
+                # draw menu text
+                self.screen.blit(menu_text, menu_text_rect)    
+                # draw FogButton
+                for Fogbutton in [
+                    zone1_FogButton,
+                    zone2_FogButton,
+                    zone3_FogButton,
+                    zone4_FogButton,
+                    zone6_FogButton,
+                    zone7_FogButton,
+                    zone8_FogButton,
+                    zone9_FogButton]:
+                    Fogbutton.changeColor(Menu_mouse_position)
+                    if Fogbutton.disable_button == False:
+                        Fogbutton.update(self.screen)
+            else:
+                self.screen.blit(show_popup, (0,0))
+                for DecisionButton in [
+                    decisio_button_leave,
+                    decisio_button_search]:
+                    DecisionButton.changeColor(Menu_mouse_position)
+                    DecisionButton.update(self.screen)
+                
              # events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -85,8 +97,8 @@ class Campaing:
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if zone1_FogButton.checkForInput(Menu_mouse_position):
-                        #draw popUp
-                        self.screen.blit(show_popup, (0,0))
+                        popup_validator = True
+                        
                     if zone2_FogButton.checkForInput(Menu_mouse_position):
                         print('play')
                     if zone3_FogButton.checkForInput(Menu_mouse_position):
@@ -105,3 +117,5 @@ class Campaing:
 
             # update
             pygame.display.flip()
+
+    #def decisionButton(self):
--- utils/fogbutton.py
+++ utils/fogbutton.py
@@ -21,6 +21,8 @@ class FogButton():
 	def checkForInput(self, position):
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			self.disable_button = True
+			return True
+		return False
 
 	def changeColor(self, position):
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
