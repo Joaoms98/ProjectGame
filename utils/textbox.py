05f2365ed016ade0bf9c60f68834da1578185e07
Joaoms98 <jvm1800@gmail.com>
2024-01-09 17:58:34 -0300

    feat: updated textbox

--- utils/textbox.py
+++ utils/textbox.py
@@ -1,20 +1,21 @@
 import pygame, textwrap
 
 class TextBox:
-    def __init__(self, screen, font_size, font):
+    def __init__(self, screen, font_size, font, size):
         self.screen = screen
         self.font_size = font_size
         self.font = font
-        self.text_rect = pygame.Rect(250, 500, 1200, 500)
+        self.size = size
 
-    def draw(self, text, color):
-        y = self.text_rect.top
+    def draw(self, text, color, pos):
+        text_rect = pygame.Rect((pos), (self.size))
+        y = text_rect.top
         line_spacing = -2
-        words = textwrap.wrap(text, width=self.text_rect.width // self.font_size)
-        
+        words = textwrap.wrap(text, width=text_rect.width // self.font_size)
+
         for word in words:
             word_surface = self.font.render(word, True, color)
             word_height = word_surface.get_height()
-            self.screen.blit(word_surface, (self.text_rect.x, y))
+            self.screen.blit(word_surface, (text_rect.x, y))
             y += word_height + line_spacing
 
