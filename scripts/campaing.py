21660dd635e4534fde9b4ef6154ed94beea01f55
Higlik <victor.silva.franca18@gmail.com>
2024-01-09 00:29:12 -0300

    Fodase essa merda

--- scripts/campaing.py
+++ scripts/campaing.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from utils.campaingbutton import CampaingButton
 import utils.language as lang
 import utils.config as config
 
@@ -19,17 +20,72 @@ class Campaing:
             self.resolution
         )
 
-    while True:
-        # set frames
-        self.clock.tick(self.fps)
-        
-        Menu_mouse_position = pygame.mouse.get_pos()
-        
-        # draw background
-        self.screen.blit(menu_background, (0, 0))
+        # menu text variables
+        font_level = pygame.font.Font("assets/fonts/alagard.ttf", 20)        
+        menu_text = font_level.render(dict_lang["campaing_text_level"], True, "#11ebeb")
+        menu_text_rect = menu_text.get_rect(center=(self.screen_rect.centerx, 50))
 
-        # draw menu text
-        # self.screen.blit(menu_text, menu_text_rect)
+        # menu text variables
+        font = pygame.font.Font("assets/fonts/alagard.ttf", 30)        
 
-        # update
-        pygame.display.flip()
+        # menu CampaingButton text variables
+        menu_text_color = "#ff0000"
+        hover_text_color = "White"
+        image = pygame.image.load("assets/buttons/zone_buttons/Zone_Layout.png")
+
+        #Zones CampaingButton
+        zone1_CampaingButton = CampaingButton(image, (150, 100), "Explorar", font, menu_text_color, hover_text_color)
+        zone2_CampaingButton = CampaingButton(image, (500, 100), "Explorar", font, menu_text_color, hover_text_color)
+        zone3_CampaingButton = CampaingButton(image, (850, 100), "Explorar", font, menu_text_color, hover_text_color)
+        zone4_CampaingButton = CampaingButton(image, (150, 350), "Explorar", font, menu_text_color, hover_text_color)
+        zone6_CampaingButton = CampaingButton(image, (850, 350), "Explorar", font, menu_text_color, hover_text_color)
+        zone7_CampaingButton = CampaingButton(image, (150, 600), "Explorar", font, menu_text_color, hover_text_color)
+        zone8_CampaingButton = CampaingButton(image, (500, 600), "Explorar", font, menu_text_color, hover_text_color)
+        zone9_CampaingButton = CampaingButton(image, (850, 600), "Explorar", font, menu_text_color, hover_text_color)
+        
+        while True:
+            # set frames
+            self.clock.tick(self.fps)
+            
+            Menu_mouse_position = pygame.mouse.get_pos()
+            
+            # draw background
+            self.screen.blit(menu_background, (0, 0))
+
+            # draw menu text
+            self.screen.blit(menu_text, menu_text_rect)
+
+             # draw CampaingButton
+            for CampaingButton in [zone1_CampaingButton, zone2_CampaingButton, zone3_CampaingButton,
+             zone4_CampaingButton,zone6_CampaingButton,zone7_CampaingButton,
+             zone8_CampaingButton,zone9_CampaingButton]:
+                CampaingButton.changeColor(Menu_mouse_position)
+                if CampaingButton.disable_button == False:
+                    CampaingButton.update(self.screen)
+             
+             # events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if zone1_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone2_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone3_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone4_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone6_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone7_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone8_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+                    if zone9_CampaingButton.checkForInput(Menu_mouse_position):
+                        print('play')
+
+
+            # update
+            pygame.display.flip()
--- utils/language.py
+++ utils/language.py
@@ -1,23 +1,31 @@
 class Language: 
     def set_lang(self, lang):
         dictionaryeng = {
+                        #main_menu
                         'menu_text_play': 'Play',
                         'menu_text_options': 'Options',
                         'menu_text_exit': 'Exit',
                         'menu_text_resolution': 'Resolution',
                         'menu_text_volume': 'Volume',
                         'menu_text_language': 'Language',
-                        'menu_text_apply': 'Apply'
+                        'menu_text_apply': 'Apply',
+                        #campaing
+                        'campaing_text_explore': 'Explore',
+                        'campaing_text_level': 'Level'
                         }
 
         dictionarypt = {
+                        #main_menu
                         'menu_text_play': 'Iniciar',
                         'menu_text_options': 'Opções',
                         'menu_text_exit': 'Sair',
                         'menu_text_resolution': 'Resolução',
                         'menu_text_volume': 'Volume',
                         'menu_text_language': 'Linguagem',
-                        'menu_text_apply': 'Aplicar'
+                        'menu_text_apply': 'Aplicar',
+                        #campaing
+                        'campaing_text_explore': 'Explorar',
+                        'campaing_text_level': 'Andar'
                         }
 
         if lang == 'eng':
