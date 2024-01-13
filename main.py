ca9aaad3b002a5d168ef7aa63fe60c9f45d37580
Joaoms98 <jvm1800@gmail.com>
2024-01-13 17:20:40 -0300

    f

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from data.Seed import Seed
+from objects.character import Character
+from scripts.Arena import Arena
 from scripts.Map1 import Map1
 from scripts.Map2 import Map2
 from scripts.menu import Menu
@@ -23,11 +25,19 @@ while True:
     # menu = Menu(screen, screen_rect, config.fps, config.resolution)
     # menu.menu()
 
-    character_select = CharacterSelect(screen, screen_rect, config.fps, config.resolution, allies)
-    team_selected = character_select.run()
+    # character_select = CharacterSelect(screen, screen_rect, config.fps, config.resolution, allies)
+    # team_selected = character_select.run()
+
+    enemy1 = Character("capiroto", 'assets/portraits/Haguddrun(Wizard).png', 'assets/portraits/Haguddrun(Wizard)_dead.png', 10,10,1,10,10,10,10,1)
+    enemy2 = Character("demonio", 'assets/portraits/Haguddrun(Wizard).png', 'assets/portraits/Haguddrun(Wizard)_dead.png', 10,10,1,10,10,10,10,1)
+    enemy3 = Character("ditocujo", 'assets/portraits/Haguddrun(Wizard).png', 'assets/portraits/Haguddrun(Wizard)_dead.png', 10,10,1,10,10,10,10,1)
+
+    arena = Arena(screen, screen_rect, config.fps, config.resolution, [allies[0], allies[1], allies[2]], [enemy1, enemy2, enemy3])
+    arena.run()
 
     # map1 = Map1(screen, screen_rect, config.fps, config.resolution, team_selected)
     # map1.run()
 
-    map2 = Map2(screen, screen_rect, config.fps, config.resolution, team_selected)
-    map2.run()
+    # map2 = Map2(screen, screen_rect, config.fps, config.resolution, team_selected)
+    # map2.run()
+
--- scripts/arena.py
+++ scripts/arena.py
@@ -14,50 +14,41 @@ class Arena:
         self.enemies = enemies
         self.clock = pygame.time.Clock()
 
-    def arena(self):
+    def run(self):
         # objects instances 
         dict_lang = lang.Language.set_lang(self, config.language)
 
-        black_image = 'assets/background/black_image.jpg'
-
-        ########## background ##############
-        #format background image
+        # background variables
         background = pygame.transform.scale(
             pygame.image.load('assets/background/background_nuvens.jpg').convert(),
             self.resolution
         )
 
-        ######### allies and enemies #########
-        Atributtes_text = TextBox(self.screen, 15, pygame.font.SysFont("arial", 15), (1000,700))
-
-        #format picture of allies and enemies        
+        # format picture of allies and enemies
         for character in self.allies:
-            character.picture = pygame.transform.scale(
-                pygame.image.load(character.picture).convert(),
-                (150, 150)
-            )
+            character.picture = pygame.image.load(f'{character.picture}').convert()
 
         for character in self.enemies:
-            character.picture = pygame.transform.scale(
-                pygame.image.load(character.picture).convert(),
-                (150, 150)
-            )
+            character.picture = pygame.image.load(f'{character.picture}').convert()
 
-        ############## prompt #############
-        #format prompt image
+        # create attributes text
+        attributes_text = self.createAttributesText()
+
+        # prompt variables
+        black_image = 'assets/background/black_image.jpg'
         prompt = pygame.transform.scale(
             pygame.image.load(black_image).convert(),
             (650, 220)
         )
 
+        # hit_button1 = Button(None, (90, 500), "attack1", font, (0,0,0), (255,0,0))
+        # hit_button2 = Button(None, (90, 550), "attack2", font, (0,0,0), (255,0,0))
+
+        # enemy_choice1 = Button(None, (900, 500), "enemie1", font, (0,0,0), (255,0,0))
+        # enemy_choice2 = Button(None, (900, 550), "enemie2", font, (0,0,0), (255,0,0))
+        # enemy_choice3 = Button(None, (900, 600), "enemie3", font, (0,0,0), (255,0,0))
+
         font = pygame.font.Font("assets/fonts/font.ttf", 15)  
-        hit_button1 = Button(None, (90, 500), "attack1", font, (0,0,0), (255,0,0))
-        hit_button2 = Button(None, (90, 550), "attack2", font, (0,0,0), (255,0,0))
-
-        enemie_choice1 = Button(None, (900, 500), "enemie1", font, (0,0,0), (255,0,0))
-        enemie_choice2 = Button(None, (900, 550), "enemie2", font, (0,0,0), (255,0,0))
-        enemie_choice3 = Button(None, (900, 600), "enemie3", font, (0,0,0), (255,0,0))
-
         exit = Button(None, (500, 100), "exit", font, (0,0,0), (255,0,0))
 
         back_to_event = False
@@ -80,7 +71,8 @@ class Arena:
                 y = 20 + (151 * allies_count)
                 self.screen.blit(character.picture, (15, y))
                 allies_count = allies_count + 1
-                Atributtes_text.draw(f"Life: {character.life} ", (255,0,0), (170, y + 20))
+                attributes_text[0].updateText(self.screen, f'Hp: {character.hp}')
+                attributes_text[1].updateText(self.screen, f'Mp: {character.mp}')
 
             # draw enemies
             enemies_count = 0
@@ -88,20 +80,19 @@ class Arena:
                 y = 20 + (151 * enemies_count)
                 self.screen.blit(character.picture, (840, y))
                 enemies_count = enemies_count + 1
-                Atributtes_text.draw(f"Life: {character.life} ", (255,0,0), (780, y + 20))
 
-            # draw attack buttom
-            hit_button1.changeColor(mouse_position)
-            hit_button1.update(self.screen)
-            hit_button2.changeColor(mouse_position)
-            hit_button2.update(self.screen)
+            # # draw attack buttom
+            # hit_button1.changeColor(mouse_position)
+            # hit_button1.update(self.screen)
+            # hit_button2.changeColor(mouse_position)
+            # hit_button2.update(self.screen)
 
-            enemie_choice1.changeColor(mouse_position)
-            enemie_choice1.update(self.screen)
-            enemie_choice2.changeColor(mouse_position)
-            enemie_choice2.update(self.screen)
-            enemie_choice3.changeColor(mouse_position)
-            enemie_choice3.update(self.screen)
+            # enemie_choice1.changeColor(mouse_position)
+            # enemie_choice1.update(self.screen)
+            # enemie_choice2.changeColor(mouse_position)
+            # enemie_choice2.update(self.screen)
+            # enemie_choice3.changeColor(mouse_position)
+            # enemie_choice3.update(self.screen)
 
             exit.update(self.screen)
 
@@ -110,23 +101,23 @@ class Arena:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if hit_button1.checkForInput(mouse_position):
-                        print('hit1')
-                        attack = 10 
-                    if hit_button2.checkForInput(mouse_position):
-                        attack = 20
-                    if enemie_choice1.checkForInput(mouse_position):
-                        enemie_choice = self.enemies[0]
-                        self.hitbutton(attack, enemie_choice)
-                    if enemie_choice2.checkForInput(mouse_position):
-                        enemie_choice = self.enemies[1]
-                        self.hitbutton(attack, enemie_choice)
-                    if enemie_choice3.checkForInput(mouse_position):
-                        enemie_choice = self.enemies[2]
-                        self.hitbutton(attack, enemie_choice)
-                    if exit.checkForInput(mouse_position):
-                        back_to_event = True
+                # if event.type == pygame.MOUSEBUTTONDOWN:
+                #     if hit_button1.checkForInput(mouse_position):
+                #         print('hit1')
+                #         attack = 10 
+                #     if hit_button2.checkForInput(mouse_position):
+                #         attack = 20
+                #     if enemie_choice1.checkForInput(mouse_position):
+                #         enemie_choice = self.enemies[0]
+                #         self.hitbutton(attack, enemie_choice)
+                #     if enemie_choice2.checkForInput(mouse_position):
+                #         enemie_choice = self.enemies[1]
+                #         self.hitbutton(attack, enemie_choice)
+                #     if enemie_choice3.checkForInput(mouse_position):
+                #         enemie_choice = self.enemies[2]
+                #         self.hitbutton(attack, enemie_choice)
+                #     if exit.checkForInput(mouse_position):
+                #         back_to_event = True
 
                 if back_to_event == True:
                     break
@@ -134,7 +125,24 @@ class Arena:
             # update
             pygame.display.flip()
 
-    def hitbutton(self, attack, enemie):
+    def createAttributesText(self):
+        font_size = 15
+        font = pygame.font.SysFont("arial", 15)
+        size_rect = (1000,700)
+        hp_text_color = (255, 0, 0)
+
+        hp_allies_1 = TextBox(font_size, font, size_rect, "Hp", hp_text_color, (600, 170))
+        mp_allies_1 = TextBox(font_size, font, size_rect, "Hp", hp_text_color, (600, 180))
+
+        hp_allies_2 = TextBox(font_size, font, size_rect, "Hp", hp_text_color, (600, 170))
+        mp_allies_2 = TextBox(font_size, font, size_rect, "Hp", hp_text_color, (600, 180))
+
+        hp_allies_3 = TextBox(font_size, font, size_rect, "Hp", hp_text_color, (600, 170))
+        mp_allies_3 = TextBox(font_size, font, size_rect, "Hp", hp_text_color, (600, 180))
+
+        return [hp_allies_1, mp_allies_1, hp_allies_2, mp_allies_2, hp_allies_3, mp_allies_3]
+
+    def hitButton(self, attack, enemie):
         enemie.life = enemie.life -attack
         
     
